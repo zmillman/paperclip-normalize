40419055c119ec2604856f5d8045928406895424
Zach Millman <zach.millman@gmail.com>
2012-05-03 11:21:05 -0700

    Initial commit

---
+++ lib/paperclip-normalize.rb
@@ -0,0 +1,7 @@
+require "paperclip-normalize/version"
+
+module Paperclip
+  module Normalize
+    # Your code goes here...
+  end
+end
---
+++ lib/paperclip-normalize/version.rb
@@ -0,0 +1,5 @@
+module Paperclip
+  module Normalize
+    VERSION = "0.0.1"
+  end
+end
